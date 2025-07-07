842b249683def90b899925e38e5823578ce37be8
islam kabbary <islamkabary@gmail.com>
2025-07-07 23:21:02 +0300

    lec 5

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,26 @@
 <?php
 
 use App\Models\Post;
-use App\Models\User;
-use App\Models\Profile;
-use App\Models\Category;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\ProfileController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
 
 /*
 // ORM (Object relational Mapping)
@@ -22,7 +34,7 @@ Post::find(1);
 //     return view('welcome');
 // });
 
-Route::get("/", [PageController::class, 'home']);
+// Route::get("/", [PageController::class, 'home']);
 Route::get("/about-us", [PageController::class, 'about']);
 Route::get("/contact-us", [PageController::class, 'contact']);
 Route::post("/contact-us", [PageController::class, 'create_new_contect'])->name('create_new_contect');
@@ -34,131 +46,53 @@ Route::get("/blog/{id}", [PageController::class, 'showBlog']);
 // Route::view("/", "home");
 // Route::view("/about-us", "about");
 
-Route::get("/profile", function () {
-    // $user = User::create([
-    //     "name" => "islam",
-    //     "email" => "islam2@gmail.com",
-    //     "password" => Hash::make(123456),
-    // ]);
-    // $user = User::find(4);
-    // $user->profile()->create([
-    //     "phone" => "0123456789",
-    //     "address" => "test",
-    // ]);
-    
-    // $user = User::find(4);
-    // dd($user->profile);
-    
-    // $profile = Profile::find(3);
-    // dd($profile->user);
+Route::get("/posts", function () {
+    // create
+    // $post = new Post();
+    // $post->title = "first title ".time();
+    // $post->content = "first content";
+    // $post->save();
+
+    // Read
+    // dd(Post::find(3)->is_published);
+    // return Post::findOrFail(1);
+    // return Post::all();
+
+    // update
+    // $post = Post::find(1);
+    // $post->title = "update First Title 1";
+    // $post->content = "update First content 1";
+    // $post->save();
+
+    // delete
+    // $post = Post::find(1);
+    // $post->delete();
 });
 
-// Route::get("/posts", function () {
-//     // $user = User::create([
-//     //     "name" => "islam",
-//     //     "email" => "islam1@gmail.com",
-//     //     "password" => Hash::make(123456),
-//     // ]);
-
-//     // $user->posts()->createMany([
-//     //     ["title" => "1 title" , "content" => "1 content"],
-//     //     ["title" => "2 title" , "content" => "2 content"],
-//     //     ["title" => "3 title" , "content" => "3 content"],
-//     // ]);
-
-//     // $post = new Post;
-//     // $post->title = "title";
-//     // $post->content = "content";
-//     // $post->user_id = 1;
-//     // $post->save();
-    
-//     $user = User::find(1);
-//     // dd($user->posts);
-
-//     foreach($user->posts as $post){
-//         echo $post->user->name . "<br>";
-//     }
-// });
-
-// Route::get("/posts", function () {
-//     // create
-//     // $post = new Post();
-//     // $post->title = "first title ".time();
-//     // $post->content = "first content";
-//     // $post->save();
-
-//     // Read
-//     // dd(Post::find(3)->is_published);
-//     // return Post::findOrFail(1);
-//     // return Post::all();
-
-//     // update
-//     // $post = Post::find(1);
-//     // $post->title = "update First Title 1";
-//     // $post->content = "update First content 1";
-//     // $post->save();
-
-//     // delete
-//     // $post = Post::find(1);
-//     // $post->delete();
-// });
-
-// Route::get("/posts", function () {
-//     // // create
-//     // Post::create([
-//     //     "title" => "first title Mass Assi 1",
-//     //     "content" => "first content Mass Assi 1",
-//     // ]);
+Route::get("/posts", function () {
+    // // create
+    // Post::create([
+    //     "title" => "first title Mass Assi 1",
+    //     "content" => "first content Mass Assi 1",
+    // ]);
 
 
 
-//     // Query
-//     // dd(Post::where('is_published',true)->get());
-//     // dd(Post::where('is_published',false)->get());
-//     // dd(Post::orderBy('created_at',"desc")->get());
-//     // dd(Post::where('is_published',true)->where("created_at",">=",now()->subDays(7))->get());
-//     // dd(Post::where('title',"like","%57%")->get());
-//     // dd(Post::limit(5)->get());
-//     // dd(Post::whereIn('id',[3,4,8])->get());
-//     dd(Post::whereNotIn('id',[3,4,8])->first());
-//     // dd(Post::first());
-// });
-
-// Route::get("/cats", function () {
-//     // $categories = [
-//     //     "html",
-//     //     "css",
-//     //     "js",
-//     //     "php",
-//     //     "mysql",
-//     //     "laravel",
-//     // ];
-
-//     // foreach($categories as $cat){
-//     //     Category::create(['name' => $cat]);
-//     // }
-
-
-//     $post = Post::find(2);
-//     // $post->categories()->detach([1,2]);
-
-//     // $cat = Category::find(5);
-//     // $cat->posts()->attach([2,3,4]);
-// });
-
-Route::get("/test", function () {
-    Post::factory(100)->create();
-    // $posts = DB::table('posts')->get();
-    // $posts = DB::table('posts')->where('id',2)->first();
-    // $posts = DB::table('posts')->insert([
-        //     'title' => 'new post query builder',
-        //     'content' => 'new post query builder',
-        //     'user_id' => 1,
-        // ]);
-    // $posts = DB::table('posts')->where('id',2)->update(['title' => 'update post query builder']);
-    // $posts = DB::table('posts')->where('id',22)->delete();
-    // dd($posts);
+    // Query
+    // dd(Post::where('is_published',true)->get());
+    // dd(Post::where('is_published',false)->get());
+    // dd(Post::orderBy('created_at',"desc")->get());
+    // dd(Post::where('is_published',true)->where("created_at",">=",now()->subDays(7))->get());
+    // dd(Post::where('title',"like","%57%")->get());
+    // dd(Post::limit(5)->get());
+    // dd(Post::whereIn('id',[3,4,8])->get());
+    dd(Post::whereNotIn('id',[3,4,8])->first());
+    // dd(Post::first());
 });
+
+// Route::get("/hello", function () {
+//     return "hello Laravel";
+// });
 
 // Route::post("/hello-post", function () {
 //     return "hello Laravel";
